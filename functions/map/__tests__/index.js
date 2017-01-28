ee7c912f6f5911682bb8e692580b00ea1f628486
Robert Smith <rbrtsmith84@googlemail.com>
2017-01-28 09:51:45 +0000

    Add map function

--- functions/map/__tests__/index.js
+++ functions/map/__tests__/index.js
@@ -1,16 +1,16 @@
 import deepFreeze from 'deep-freeze'
 import map from '../'
 
+const fn = str => str.toUpperCase()
+
 describe('Map', () => {
   it('takes an array and a function and returns a new array with items transformed by the function', () => {
-    const fn = str => str.toUpperCase()
     const input = deepFreeze(['h', 'e', 'l', 'l', 'o'])
     const actual = map(input, fn)
     const expected = ['H', 'E', 'L', 'L', 'O']
     expect(actual).toEqual(expected)
   })
   it('returns an error message if the first argument is not an array', () => {
-    const fn = str => str.toUpperCase()
     const input = 'hello'
     const actual = map(input, fn)
     const expected = 'The first argument must be an array.'
